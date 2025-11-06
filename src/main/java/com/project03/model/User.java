d88dc376309ed089ebdd2f9aeb9fbae58dd65f27
hugo-severino04 <hugo.severino2014@gmail.com>
2025-11-05 18:33:27 -0800

    added users
    just for testing will replace later

---
+++ src/main/java/com/project03/model/User.java
@@ -0,0 +1,55 @@
+package com.project03.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Simple User entity for basic user management.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    // Constructors
+    public User() {
+    }
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
