ff9cb83bc001f828625aa8aca41683728c93f4ee
hugo-severino04 <hugo.severino2014@gmail.com>
2025-11-20 12:50:30 -0800

    Fix?
    Maybe lets see nd deploy

--- src/main/java/com/project03/model/StudentPreference.java
+++ src/main/java/com/project03/model/StudentPreference.java
@@ -1,5 +1,6 @@
 package com.project03.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 /**
@@ -16,6 +17,7 @@ public class StudentPreference {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @JsonIgnore
     private User user;
 
     @Column(nullable = false)
